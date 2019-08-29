58135a7a89384a049c2dc8846c5fd0fc38d635df
Administrator <Administrator@RL-20180223IJIO.mshome.net>
2019-08-29 16:49:41 +0800

    turefirst

--- work/src/main/java/controller/firstController.java
+++ work/src/main/java/controller/firstController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class firstController {
+	
+	@RequestMapping("index")
+	public String index(ModelMap m) {
+		m.put("first", "FIRST");
+		return "index";
+	}
 
 }
